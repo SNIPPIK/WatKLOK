12c8458982cff76ae5173368856f53d42f4be572
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2023-01-26 09:50:46 +0300

    Renameing to AudioPlayer/APIs

--- src/Structures/Platforms/index.ts
+++
@@ -1,7 +0,0 @@
-import {YandexMusic} from "./API/YandexMusic";
-import {SoundCloud} from "./API/SoundCloud";
-import {YouTube} from "./YouTube/YouTube";
-import {Spotify} from "./API/Spotify";
-import {VK} from "./API/VK";
-
-export {YouTube, Spotify, SoundCloud, VK, YandexMusic};
