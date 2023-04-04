5080a4d986c0a8b37fdb3bb6aa3e21372a5f801c
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2023-04-04 08:13:21 +0300

    Delete src/Structures/AudioPlayer directory

--- src/Structures/AudioPlayer/APIs/index.ts
+++
@@ -1,7 +0,0 @@
-import { YandexMusic } from "./YandexMusic";
-import { SoundCloud } from "./SoundCloud";
-import { YouTube } from "./YouTube";
-import { Spotify } from "./Spotify";
-import { VK } from "./VK";
-
-export { YouTube, Spotify, SoundCloud, VK, YandexMusic };
