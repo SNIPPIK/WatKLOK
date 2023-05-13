0ef3f7046c96ae73b55abe04d614c192fb2e50cc
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2023-05-13 10:21:49 +0300

    Delete resource directory

--- resource/env.ts
+++
@@ -1,8 +0,0 @@
-//====================== ====================== ====================== ======================
-require("dotenv").config();
-/**
- * @description Env
- */
-export namespace env {
-    export function get(name: string): string { return process.env[name]; }
-}
