717e9a21fe2dad59b53d1228b57d61b0714db9a2
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2023-06-26 01:35:05 +0300

    New logger

--- resource/Structures/Logger.ts
+++ resource/Structures/Logger.ts
@@ -1,40 +1,36 @@
-import { Duration } from "@Utils/Durations";
-
-class Logger_ {
-    private get time() {
-        const date = new Date();
-        const time = [date.getHours(), date.getMinutes(), date.getSeconds()].map(Duration.toSplit).join(":");
-        let ms = `${date.getMilliseconds()}`;
-
-        if (ms.length === 1) ms = `00${ms}`;
-        else if (ms.length === 2) ms = `0${ms}`;
-
-        return `[${time}.${ms}]`;
-    };
-    //====================== ====================== ====================== ======================
+export const Logger = new class {
     /**
      * @description Отправляем лог с временем
      * @param text {string} Текст лога
      */
     public readonly log = (text: string) => {
-        process.stdout.write(`${this.time} ➜ ${text.replace(/\[/gi, "[\x1b[32m").replace(/]/gi, "\x1b[0m]")}\n`);
+        this.sendLog(`\x1b[36mi\x1b[0m`, text);
     };
-    //====================== ====================== ====================== ======================
+
+
     /**
      * @description Отправляем лог с временем и красным текстом
      * @param text {string} Текст лога
      */
-    public readonly error = (text: string): void => {
-        process.stdout.write(`${this.time} ➜ [\x1b[31mError\x1b[0m]: \x1b[31m${text}\x1b[0m\n`);
-    }
-    //====================== ====================== ====================== ======================
+    public readonly error = (text: string) => {
+        this.sendLog(`\x1b[41m \x1b[30mERROR \x1b[0m`, `\x1b[31m${text}\x1b[0m`, 5);
+    };
+
+
     /**
      * @description Отправляем лог с временем
      * @param text {string} Текст лога
      */
-    public readonly debug = (text: string): void => {
-        process.stdout.write(`${this.time} ➜ [\x1b[33mDebug\x1b[0m]: ${text.replace(/\[/gi, "[\x1b[32m").replace(/]/gi, "\x1b[0m]")}\n`);
-    }
-}
+    public readonly debug = (text: string) => {
+        this.sendLog(`\x1b[46m \x1b[30mDEBUG \x1b[0m`, `\x1b[90m${text}\x1b[0m`, 14);
+    };
 
-export const Logger = new Logger_();
+
+    private sendLog = (status: string, text: string, supNum: number = 0) => {
+        const time = `\x1b[90m${new Date().toLocaleTimeString()}\x1b[0m`;
+        const spaces = 135 - (status.length + text.length) - (time.length - supNum);
+
+        if (spaces < 0) process.stdout.write(`${status} ${text} \n`);
+        else process.stdout.write(`${status} ${text} ${" ".repeat(spaces)}${time} \n`);
+    };
+}
