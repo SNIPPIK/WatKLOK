5534b7202d6ebe13cc3d97bacf2cdb222471969b
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2023-08-08 17:25:02 +0300

    Delete src directory

--- src/Actions/Client/Reconecting.ts
+++
@@ -1,8 +0,0 @@
-import {Events} from "discord.js";
-import { Action } from "@Action";
-import { Logger } from "@Logger";
-
-export default class extends Action {
-    public readonly name = Events.ShardReconnecting;
-    public readonly run = (ShardID: string): void => Logger.log(`[Shard ${ShardID}] reconnecting...`);
-}
