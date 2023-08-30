d0b26aa6048778a60660f93ef42f53db9dee85bc
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2023-08-30 09:55:26 +0300

    Delete src directory

--- src/Handlers/Actions/Client/Disconecting.ts
+++
@@ -1,8 +0,0 @@
-import {Events} from "discord.js";
-import { Action } from "@Action";
-import { Logger } from "@Logger";
-
-export default class extends Action {
-    public readonly name = Events.ShardDisconnect;
-    public readonly execute = (ShardID: string): void => Logger.log(`[Shard ${ShardID}] disconnecting...`);
-}
