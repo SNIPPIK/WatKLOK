3ebd85168d4e85b4e65fa4f986372be23f673b2a
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2024-06-28 18:11:28 +0300

    Исправление ошибки queue.songs.last

--- src/handlers/Events/Player/message.ts
+++ src/handlers/Events/Player/message.ts
@@ -17,6 +17,9 @@ class onError extends Constructor.Assign<Handler.Event<"message/error">> {
             name: "message/error",
             type: "player",
             execute: (queue, error) => {
+                //Если больше нет треков в очереди
+                if (queue.songs.size < 1) return;
+                
                 const {color, author, image, title, requester} = queue.songs.last;
 
                 new MessageBuilder().addEmbeds([
